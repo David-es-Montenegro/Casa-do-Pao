6a62545cea9a12d0f1541a9ae802e0a2bb1e6427
David-es-Montenegro <115959360+David-es-Montenegro@users.noreply.github.com>
2023-06-14 07:24:20 -0300

    Update tabela.js

--- Js/tabela.js
+++ Js/tabela.js
@@ -7,13 +7,13 @@ function preencherTabela(dados) {
         var row = tabela.insertRow(i + 1); // Inicia a partir da linha 1 para não substituir o cabeçalho
   
         // Preenche as células com os dados do beneficiário
-        row.insertCell(0).textContent = beneficiario.id;
+        row.insertCell(0).textContent = beneficiario.id_usuario;
         row.insertCell(1).textContent = beneficiario.nome;
         row.insertCell(2).textContent = beneficiario.genero;
         row.insertCell(3).textContent = beneficiario.cpf;
-        row.insertCell(4).textContent = beneficiario.dataNascimento;
+        row.insertCell(4).textContent = beneficiario.data_nascimento;
         row.insertCell(5).textContent = beneficiario.cep;
-        row.insertCell(6).textContent = beneficiario.eixoFormacao;
+        row.insertCell(6).textContent = beneficiario.eixo_formacao;
     }
 }
 
