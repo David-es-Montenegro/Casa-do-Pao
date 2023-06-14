00bd598ee07be4576975e1d9f44e1f1036e6c96e
David-es-Montenegro <115959360+David-es-Montenegro@users.noreply.github.com>
2023-06-14 05:13:19 -0300

    Update form.js

--- Js/form.js
+++ Js/form.js
@@ -4,18 +4,27 @@ function enviarFormulario() {
     var xhr = new XMLHttpRequest();
     xhr.open('POST', 'https://casadopao.pythonanywhere.com/login');
     xhr.onload = function () {
-      var popup = document.getElementById('popup');
-      var closeButton = document.querySelector('.close-button');
-      var responseMessage = document.getElementById('responseMessage');
-      
-      responseMessage.textContent = xhr.responseText;
-      popup.style.display = 'block';
-      
-      closeButton.addEventListener('click', function() {
-        popup.style.display = 'none';
-      });
+        var popup = document.getElementById('popup');
+        var closeButton = document.querySelector('.close-button');
+        var responseMessage = document.getElementById('responseMessage');
+
+        if (xhr.status === 200) {
+            // Código 200 indica uma resposta bem-sucedida
+            window.location.href = "/index.html";
+        } else if (xhr.status === 401) {
+            // Código 401 indica uma resposta de autorização inválida
+            responseMessage.textContent = "Erro de autorização. Por favor, faça login novamente.";
+            popup.style.display = 'block';
+        } else {
+            // Outros códigos de status podem ser tratados aqui
+            responseMessage.textContent = "Erro desconhecido.";
+            popup.style.display = 'block';
+        }
+
+        closeButton.addEventListener('click', function () {
+            popup.style.display = 'none';
+        });
     };
     xhr.setRequestHeader('chave', 'teste');
     xhr.send(formData);
-  }
-  
+}
