fb57a7161e20f78e712154e92ce556d401089a47
David-es-Montenegro <115959360+David-es-Montenegro@users.noreply.github.com>
2023-07-30 22:42:11 -0300

    Adicionadas verificações.
    Formatação dos dados feita corretamente para enviar para a planilha ou consultas.
    Adicionadas verificações de dados no frontend. (CPF e RG)

--- Js/form.js
+++ Js/form.js
@@ -72,7 +72,7 @@ function enviarFormBeneficiario() {
     if (xhr.status === 201) {
       // A requisição foi bem-sucedida
       if (xhr.responseText === 'Sucesso') {
-        console.log('Dados enviados com sucesso');
+        alert('Cadastro realizado com sucesso!');
       } else {
         alert('Erro ao enviar os dados');
       }
@@ -84,14 +84,120 @@ function enviarFormBeneficiario() {
   xhr.send(formData);
 }
 
-// function previewImage(event) {
-//   var input = event.target;
-//   if (input.files && input.files[0]) {
-//     var reader = new FileReader();
-//     reader.onload = function(e) {
-//       var preview = document.getElementById('preview');
-//       preview.src = e.target.result;
-//     };
-//     reader.readAsDataURL(input.files[0]);
-//   }
-// }
+document.getElementById('cpf').addEventListener('input', formatarCPF);
+
+function formatarCPF() {
+  var cpf = document.getElementById('cpf').value.replace(/[a-zA-Z]/g, '');
+
+  switch (cpf.length) {
+    case 0:
+      break;
+    case 3:
+      cpf = cpf.slice(0, 3) + '.'; // Adiciona o ponto após os primeiros 3 dígitos
+      break;
+    case 7:
+      cpf = cpf.slice(0, 7) + '.'; // Adiciona o ponto após os primeiros 6 dígitos
+      break;
+    case 11:
+      cpf = cpf.slice(0, 11) + '-'; // Adiciona o traço após os primeiros 9 dígitos
+      break;
+    case 14:
+      break;
+  }
+
+  document.getElementById('cpf').value = cpf;
+}
+
+function validarCPF() {
+    // Obtenha o valor digitado pelo usuário no campo de CPF
+    const cpf = document.getElementById('cpf').value;
+
+    // Remova caracteres especiais (pontuação e espaços) do CPF
+    const cpfLimpo = cpf.replace(/[^\d]/g, '');
+
+    if (cpfLimpo.length == 0) {
+      return;
+    }
+    // Verifique se o CPF tem 11 dígitos
+    if (cpfLimpo.length !== 11) {
+      alert('CPF invalido, o CPF deve ter 11 digitos.')
+      document.getElementById('cpf').value = '';
+      return;
+    }
+    // Verifique se todos os dígitos são iguais (caso inválido)
+    if (/^(\d)\1+$/.test(cpfLimpo)) {
+      alert('CPF invalido, todos os digitos são iguais.')
+      document.getElementById('cpf').value = '';
+      return;
+    }
+
+    // Verifique os dígitos verificadores
+    let soma = 0;
+    let resto;
+    for (let i = 1; i <= 9; i++) {
+      soma += parseInt(cpfLimpo.substring(i - 1, i)) * (11 - i);
+    }
+    resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) resto = 0;
+    if (resto !== parseInt(cpfLimpo.substring(9, 10))) {
+      alert('CPF invalido, este CPF não é real.')
+      document.getElementById('cpf').value = '';
+      return;
+    }
+
+    soma = 0;
+    for (let i = 1; i <= 10; i++) {
+      soma += parseInt(cpfLimpo.substring(i - 1, i)) * (12 - i);
+    }
+    resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) resto = 0;
+    if (resto !== parseInt(cpfLimpo.substring(10, 11))) {
+      alert('CPF invalido, este CPF não é real.')
+      document.getElementById('cpf').value = '';
+      return;
+    }
+  }
+
+document.getElementById('rg').addEventListener('input', formatarRG);
+
+function formatarRG() {
+  var rg = document.getElementById('rg').value.replace(/[a-zA-Z]/g, '');
+
+  switch (rg.length) {
+    case 0:
+      break;
+    case 2:
+      rg = rg.slice(0, 2) + '.'; // Adiciona o ponto após os primeiros 3 dígitos
+      break;
+    case 6:
+      rg = rg.slice(0, 6) + '-'; // Adiciona o ponto após os primeiros 6 dígitos
+      break;
+  }
+
+  document.getElementById('rg').value = rg;
+}
+
+function validarRG() {
+  // Obtenha o valor digitado pelo usuário no campo de RG
+  const rg = document.getElementById('rg').value;
+
+  // Remova caracteres especiais (pontuação e espaços) do RG
+  const rgLimpo = rg.replace(/[^\d]/g, '');
+
+  if (rgLimpo.length === 0) {
+    return;
+  }
+
+  // Verifique se todos os dígitos são iguais (caso inválido)
+  if (/^(\d)\1+$/.test(rgLimpo)) {
+    alert('RG inválido, todos os dígitos são iguais.');
+    document.getElementById('rg').value = '';
+    return;
+  }
+
+  if (rgLimpo.length < 8) {
+    alert('RG inválido, o RG deve ter pelo menos 8 dígitos.');
+    document.getElementById('rg').value = '';
+    return;
+  }
+}
